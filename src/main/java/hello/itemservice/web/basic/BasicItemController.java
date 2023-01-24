922bba709df03013c6c031cc4ed7e4e6fafd2f90
jjdone <ehdlfaos@gmail.com>
2023-01-24 16:03:25 +0900

    상품 등록 폼 구현

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -34,6 +34,16 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "basic/addForm";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
